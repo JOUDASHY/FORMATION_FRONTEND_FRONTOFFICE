76a4c9a10de4a5c6a1372c9e8a31e820c4a4c74d
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-01-30 07:47:23 +0300

    Ajout de _redirects pour Render

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: './',
   define: {
     global: {},
   },
-  server: {
-    host: '0.0.0.0',  // Écoute sur toutes les interfaces réseau
-    port: 5174,        // Ou un autre port de votre choix
-    open: true,        // Ouvre automatiquement le navigateur (optionnel)
-    historyApiFallback: true, // Permet le fallback vers index.html
-  },
   build: {
-    outDir: 'dist', // Le répertoire de sortie pour le build
-  }
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5174,
+    open: true,
+  },
 });
